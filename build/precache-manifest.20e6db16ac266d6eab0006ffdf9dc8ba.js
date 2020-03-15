587240e29ea009e386ca9759795b8e3d8509284d
Vojtěch Tranta <vojta.tranta@gmail.com>
2020-03-15 19:16:14 +0100

    update build paths

---
+++ build/precache-manifest.20e6db16ac266d6eab0006ffdf9dc8ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4dd1537573e6af78221cb371c75a33",
+    "url": "/savings-calculator/build/index.html"
+  },
+  {
+    "revision": "a21d00a7fe20954bf536",
+    "url": "/savings-calculator/build/static/css/main.9c1c1b9b.chunk.css"
+  },
+  {
+    "revision": "63aa1d216f4332e159d9",
+    "url": "/savings-calculator/build/static/js/2.08187072.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/savings-calculator/build/static/js/2.08187072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21d00a7fe20954bf536",
+    "url": "/savings-calculator/build/static/js/main.e43a6e39.chunk.js"
+  },
+  {
+    "revision": "af17397eaa6118497cad",
+    "url": "/savings-calculator/build/static/js/runtime-main.cd882055.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.360943382235f25da4b895cfae0fe634.js"
+  "/savings-calculator/build/precache-manifest.20e6db16ac266d6eab0006ffdf9dc8ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/savings-calculator/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
